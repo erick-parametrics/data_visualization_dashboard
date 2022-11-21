4993bbf83bd55a1a0c213629db0f0e64566cdbaf
Erick Nuque <erick@parametrics.ag>
2022-11-21 11:39:25 -0600

    start the calendar components - install dependencies required

--- data_visualization/src/App.js
+++ data_visualization/src/App.js
@@ -8,7 +8,7 @@ import Topbar from "./scenes/global/Topbar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Claims from "./scenes/claims";
-// import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar";
 import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 // import Bar from "./scenes/bar";
@@ -34,12 +34,12 @@ const App = () => {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/claims" element={<Claims />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
             </Routes>
           </main>
         </div>
---
+++ data_visualization/src/scenes/calendar/index.jsx
@@ -0,0 +1,15 @@
+// This is built using full calendar library
+// Check documentation for reference -  www.fullcalendar.io
+
+import React, { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+
+const Calendar = () => {
+  return <div>Calendar</div>;
+};
+
+export default Calendar;
