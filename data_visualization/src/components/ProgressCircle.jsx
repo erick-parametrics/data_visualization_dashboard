559d5329f465b289eff5d43bc3b3ce9e98466767
Erick Nuque <erick@parametrics.ag>
2022-11-24 20:34:01 -0600

    main dashboard complete

--- data_visualization/src/components/ProgressCircle.jsx
+++ data_visualization/src/components/ProgressCircle.jsx
@@ -1,4 +1,29 @@
-import React from "react";
+// import React from "react";
+// import { Box, useTheme } from "@mui/material";
+// import { tokens } from "../theme";
+
+// const ProgressCircle = ({ progress = "0.75", size = "40" }) => {
+//   const theme = useTheme();
+//   const colors = tokens(theme.palette.mode);
+//   const angle = progress * 360;
+
+//   return (
+//     <Box
+//       sx={{
+//         // This can be replaced with actual meter for real time update.
+//         background: `radial-gradient(${colors.primary[400]} 55%, transparent 56%),
+//             conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360),
+//             ${colors.greenAccent[500]}`,
+
+//         borderRadius: "50%",
+//         width: `${size}px`,
+//         height: `${size}px`,
+//       }}
+//     />
+//   );
+// };
+
+// export default ProgressCircle;
 import { Box, useTheme } from "@mui/material";
 import { tokens } from "../theme";
 
@@ -6,15 +31,12 @@ const ProgressCircle = ({ progress = "0.75", size = "40" }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const angle = progress * 360;
-
   return (
     <Box
       sx={{
-        // This can be replaced with actual meter for real time update.
         background: `radial-gradient(${colors.primary[400]} 55%, transparent 56%),
-            conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360),
+            conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360deg),
             ${colors.greenAccent[500]}`,
-
         borderRadius: "50%",
         width: `${size}px`,
         height: `${size}px`,
