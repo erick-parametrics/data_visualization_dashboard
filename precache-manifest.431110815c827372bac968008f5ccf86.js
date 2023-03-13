99bdab451deda41cc2e111afc84bdb1153d15dd7
Erick Nuque <erick@parametrics.ag>
2023-03-13 14:43:57 -0600

    Updates

---
+++ precache-manifest.431110815c827372bac968008f5ccf86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cca489c5f0e02f303241",
+    "url": "/data_visualization_dashboard/static/css/main.39ebdb58.chunk.css"
+  },
+  {
+    "revision": "cca489c5f0e02f303241",
+    "url": "/data_visualization_dashboard/static/js/main.cca489c5.chunk.js"
+  },
+  {
+    "revision": "77eca6901a3d3919a9c3",
+    "url": "/data_visualization_dashboard/static/css/1.2a6452b3.chunk.css"
+  },
+  {
+    "revision": "77eca6901a3d3919a9c3",
+    "url": "/data_visualization_dashboard/static/js/1.77eca690.chunk.js"
+  },
+  {
+    "revision": "e96ca3341c1397aa9c88",
+    "url": "/data_visualization_dashboard/static/js/runtime~main.e96ca334.js"
+  },
+  {
+    "revision": "679708ca66277eb25d054074ccd56846",
+    "url": "/data_visualization_dashboard/index.html"
+  }
+];
